e385ae730f861f7f7d3765bc36a1773d711b4a7e
javiermoreno18 <72530066+javiermoreno18@users.noreply.github.com>
2020-10-14 17:43:27 -0700

    Add files via upload

---
+++ PracticaContadores_Moreno_Martinez/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PracticaContadores
+{
+    partial class Generador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPantalla = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPantalla
+            // 
+            this.tbPantalla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPantalla.Location = new System.Drawing.Point(62, 34);
+            this.tbPantalla.Multiline = true;
+            this.tbPantalla.Name = "tbPantalla";
+            this.tbPantalla.Size = new System.Drawing.Size(217, 50);
+            this.tbPantalla.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(62, 108);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(86, 55);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar número ";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBorrar.Location = new System.Drawing.Point(193, 108);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(86, 55);
+            this.btnBorrar.TabIndex = 3;
+            this.btnBorrar.Text = "Borrar ";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Generador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 208);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.tbPantalla);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Generador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPantalla;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ PracticaContadores_Moreno_Martinez/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaContadores
+{
+    public partial class Generador : Form
+    {
+        int contador = 1;
+        public Generador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (contador <= 25)
+            {
+                tbPantalla.Text = Convert.ToString(contador);
+                contador += 1;
+            }
+            else
+            {
+                MessageBox.Show("Ya se han generado 25 números", "Advertencia");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (tbPantalla.Text == "" || tbPantalla.Text == null)
+            {
+                MessageBox.Show("No hay ningún número para borrar", "Advertencia");
+            }
+            else
+            {
+                tbPantalla.Text = "";
+            }
+        }
+    }
+}
